2e5c385c57806e2bc43b217f05fc57661646e4e2
Keyla-Aparicio <keylaaparicio18@gmail.com>
2025-05-12 17:42:01 -0700

    Implementación de un Api

---
+++ src/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const searchImages = async(term) => {
+    const url = 'https://api.unsplash.com/search/photos/?client_id=kjg7B78ji34KHakkD5uhLtTi7XVAjr6WJ_b1ODqnBBI'
+    
+    const response = await axios.get(url, {
+        params:{
+            query: term
+        }
+    })
+
+    console.log(response)
+    return response.data.results
+}
+
+export default searchImages
