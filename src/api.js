c973ce86eab39dc95e9aefc85121d6016222c789
Keyla-Aparicio <keylaaparicio18@gmail.com>
2025-05-14 13:25:43 -0700

    Cambio nuevo

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const searchImages = async(term) => {
-    const url = 'https://api.unsplash.com/search/photos/?client_id=kjg7B78ji34KHakkD5uhLtTi7XVAjr6WJ_b1ODqnBBI'
+    const url = 'https://api.unsplash.com/search/photos/?client_id=Uhc-vIjF0klYQGwMGQ0sXrQxFE490Z7fntyqumHQih0'
 
     const response = await axios.get(url, {
         params:{
